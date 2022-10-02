058a431d1133887c3cd8c655f0c77094ab5a4c4e
marc.ranchin <marc.ranchin@mylittlefintech.com>
2022-10-03 00:06:10 +0200

    adds tests

--- Tests/GeoServices/GeoTests.cs
+++ Tests/GeoServices/GeoTests.cs
@@ -10,6 +10,8 @@ public class GeoTests
         var geoLocalizationService = new GeoLocalizationService();
 
         var coordinates = geoLocalizationService.GeoLocalizeAsync("Yerres").WaitAsync(CancellationToken.None).Result;
+        Assert.Equal(2.49134, coordinates.features[0].geometry.coordinates[0], 5);
+        Assert.Equal(48.71088, coordinates.features[0].geometry.coordinates[1], 5);
         coordinates = geoLocalizationService.GeoLocalizeAsync("Soisy").WaitAsync(CancellationToken.None).Result;
         Assert.Equal(2.30165, coordinates.features[0].geometry.coordinates[0], 5);
         Assert.Equal(48.98868, coordinates.features[0].geometry.coordinates[1], 5);
--- Tests/GeoServices/ItineraryTests.cs
+++ Tests/GeoServices/ItineraryTests.cs
@@ -20,5 +20,18 @@ public class ItineraryTests
 
         var result = itineraryService.ComputeItinerary(firstCoordinates, secondCoordinates)
             .WaitAsync(CancellationToken.None).Result;
+
+        Assert.Equal("meter", result.distanceUnit);
+        Assert.Equal(50601.90000000000, result.distance, 1);
+        Assert.Equal(38.3117, result.duration, 4);
+        Assert.Equal("minute", result.timeUnit);
+
+
+        //MyLogger.Information($"Distance {root.distance} {root.distanceUnit} Durée {root.duration} {root.timeUnit}");
+
+        //foreach (var step in root.portions[0].steps)
+        //{
+        //    MyLogger.Information($"{step.instruction} {step.attributes.name} Distance : {step.distance} Durée : {step.duration}");
+        //}
     }
 }
