4aad10a966bb65c31b14e0b9c380d3e1054f817c
marc.ranchin <marc.ranchin@mylittlefintech.com>
2022-10-03 09:16:26 +0200

    adds tests

--- Tests/GeoServices/GeoTests.cs
+++ Tests/GeoServices/GeoTests.cs
@@ -10,9 +10,12 @@ public class GeoTests
         var geoLocalizationService = new GeoLocalizationService();
 
         var coordinates = geoLocalizationService.GeoLocalizeAsync("Yerres").WaitAsync(CancellationToken.None).Result;
+        Assert.NotNull(coordinates);
         Assert.Equal(2.49134, coordinates.features[0].geometry.coordinates[0], 5);
         Assert.Equal(48.71088, coordinates.features[0].geometry.coordinates[1], 5);
+
         coordinates = geoLocalizationService.GeoLocalizeAsync("Versailles").WaitAsync(CancellationToken.None).Result;
+        Assert.NotNull(coordinates);
         Assert.Equal(2.13132, coordinates.features[0].geometry.coordinates[0], 5);
         Assert.Equal(48.80302, coordinates.features[0].geometry.coordinates[1], 5);
     }
